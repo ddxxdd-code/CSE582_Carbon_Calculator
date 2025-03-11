2f69781f4059b258948cec4d1438eaf33bd1d61a
Yuyao Wang <yuyao6@cs.washington.edu>
2025-03-11 14:55:15 -0700

    feat: args in main

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
+import argparse
+import yaml
+
 import carbon_accountant as accountant
 import data_loader as loader
 import system_components as components
 import workload_process as workload
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Calculate the carbon footprint of a workload.")
+    parser.add_argument("--csv_path", type=str, default="component_carbon_data.csv",
+                        help="Path to the CSV file containing component carbon data.")
+    parser.add_argument("--electricity_carbon_density", type=float, default=0.68,
+                        help="electricity carbon density (kg CO₂ per kWh)")
+    parser.add_argument("--usage", type=str, default="example_usage.yaml")
+    return parser.parse_args()
+
 def main():
-    # Example CSV file path
-    csv_path = 'component_carbon_data.csv'
+    args = parse_args()
     
     # Step 1: Load component data
-    data_loader = loader.CarbonDataLoader(csv_path)
+    data_loader = loader.CarbonDataLoader(args.csv_path)
     components = data_loader.load_components()
     
-    # Step 2: Define a workload
+    # Step 2: Define a workload using a YAML file
     # Example usage: 20h 50% CPU, 10h 20% Disk, 40h 100% GPU, 2h 5% Network
-    usage = {"CPU_EPYC7734": (20, 50), "SSD_Samsung_16TB": (10, 20), "GPU_A100": (40, 100), "NIC_ConnectX6": (2, 5)}
+    with open(args.usage, 'r') as file:
+        usage = yaml.safe_load(file)
     procedure = workload.Workload(usage)
     
-    # Step 3: Input electricity carbon density (kg CO₂ per kWh)
-    electricity_carbon_density = 0.68  # for example, 0.68 kg CO₂/kWh
-    
-    # Step 4: Calculate carbon totals
-    calculator = accountant.CarbonCalculator(components, electricity_carbon_density)
+    # Step 3: Calculate carbon totals
+    calculator = accountant.CarbonCalculator(components, args.electricity_carbon_density)
     embodied_total, operational_total = calculator.calculate_totals(procedure)
     
-    # Step 5: Display the results
+    # Step 4: Display the results
     print(f"Total Allocated Embodied Carbon: {embodied_total:.2f} kg CO₂-eq")
     print(f"Total Operational Carbon: {operational_total:.2f} kg CO₂-eq")
 
--- workload_process.py
+++ workload_process.py
@@ -4,4 +4,4 @@ class Workload:
         usage_dict: A dictionary mapping component names to usage hours and avg utilization (percentage).
         Example: {"CPU": (20, 100), "Disk": (20, 50), "GPU": (10, 100), "Network": (10, 10)}
         """
-        self.usage = usage_dict
+        self.usage = {device: (info["hours"], info["utilization"]) for device, info in usage_dict.items()}
